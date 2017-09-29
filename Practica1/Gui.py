35f046a93418ce9e7323f1bce64263fb4a27ccde
Ivan Garcia <iesivangarcia@gmail.com>
2017-09-29 15:49:44 +0200

    Functions to calculate the possible movements added

--- Practica1/Gui.py
+++ Practica1/Gui.py
@@ -1,5 +1,6 @@
 import Terrain
 
+movements = ["UP", "DOWN", "RIGHT", "LEFT"]
 
 class Gui:
     def __init__(self):
@@ -32,6 +33,48 @@ class Gui:
                 string += terrain[i][j] + " "
             string += "\n"
         print(string)
+
+    """
+    Coordinate system chosen
+            y
+            ^
+            |
+            |
+            |---------> x
+    """
+    def is_possible_movement(self, terrain, tractor_x, tractor_y, movement):
+        possible = False
+        if movement == 'RIGHT':
+            possible = tractor_x + 1 < len(terrain)
+        elif movement == 'LEFT':
+            possible = tractor_x - 1 >= 0
+        elif movement == 'UP':
+            possible = tractor_y - 1 >= 0
+        elif movement == 'DOWN':
+            possible = tractor_y + 1 < len(terrain[0])
+        return possible
+
+    def quantity_ground_to_transfer(self, terrain, tractor_x, tractor_y, ground_desired_in_cell):
+        ground_excess = terrain[tractor_y][tractor_x] - ground_desired_in_cell
+        if ground_excess < 0:
+            ground_excess = 0
+        return ground_excess
+
+    def get_all_movement_possibles(self, terrain, tractor_x, tractor_y):
+        possibles = []
+        for movement in movements:
+            is_possible = self.is_possible_movement(terrain, tractor_x, tractor_y, movement)
+            if is_possible:
+                if movement == 'RIGHT':
+                    possibles.append((tractor_x + 1, tractor_y))
+                elif movement == 'LEFT':
+                    possibles.append((tractor_x - 1, tractor_y))
+                elif movement == 'UP':
+                    possibles.append((tractor_x, tractor_y - 1))
+                elif movement == 'DOWN':
+                    possibles.append((tractor_x, tractor_y + 1))
+        return possibles
+
 
 gui = Gui()
 terrain, terrain_obj = gui.read_file()
